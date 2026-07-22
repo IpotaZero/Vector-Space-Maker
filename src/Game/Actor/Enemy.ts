4e2faf0fc2ebc474aeeee9d5ca769ee4b5757c51
ipota <kiratoki512810@gmail.com>
2026-07-23 07:35:55 +0900

    q

--- src/Game/Actor/Enemy.ts
+++ src/Game/Actor/Enemy.ts
@@ -1,26 +1,39 @@
-import { Vec } from "@ipota/vec"
+import { vec, Vec } from "@ipota/vec"
 import { T } from "../../T"
 import { Actor } from "./Actor"
 import { Ease } from "@ipota/functions"
 
 export abstract class Enemy extends Actor {
+    private shakeP = vec(0, 0)
+
     draw(ctx: CanvasRenderingContext2D) {
         ctx.strokeStyle = "black"
         ctx.beginPath()
-        ctx.arc(this.p.x, this.p.y, this.r, 0, T)
+        ctx.arc(this.p.x + this.shakeP.x, this.p.y + this.shakeP.y, this.r, 0, T)
         ctx.stroke()
     }
 
     hit() {
         this.addScript(this.hitG.bind(this))
+        this.addScript(this.shakeG.bind(this))
     }
 
     private *hitG() {
-        const r = this.r
+        const r = 48
         const frame = 6
 
         for (let i = 1; i < frame + 1; i++) {
             this.r = r - r * Ease.Out(1 - i / frame) * 0.1
+            yield
+        }
+    }
+
+    private *shakeG() {
+        const frame = 10
+
+        for (let i = 1; i < frame + 1; i++) {
+            this.shakeP.x = Math.sin(i) * Ease.Out(i / frame)
+            this.shakeP.x = Math.cos(i * 2) * Ease.Out(i / frame)
             yield
         }
     }
